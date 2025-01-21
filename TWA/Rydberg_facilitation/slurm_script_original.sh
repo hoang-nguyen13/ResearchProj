b37cfe5b66f16f1d58cb976b30cf71fea94254e7
Hoang Long Nguyem <quw51vuk@aldor.physik.uni-kl.de>
2025-01-21 16:21:34 +0100

    errout

--- TWA/Rydberg_facilitation/slurm_script_original.sh
+++ TWA/Rydberg_facilitation/slurm_script_original.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-#SBATCH --array=1-1
-#SBATCH -J Waveguide
+#SBATCH -J Rydberg
 #SBATCH -c 16
 #SBATCH -t 50:00:00
 #SBATCH -p epyc-256
 #SBATCH -e err/%x_%A.err
 #SBATCH -o out/%x_%A.out
 #SBATCH --mem 240G
-
-
-# Reihenfolge Paramter: Omega nAtoms 
-~/julia-1.11.2/bin/julia -t auto waveguide.jl $SLURM_ARRAY_TASK_ID
+ 
+~/julia-1.11.2/bin/julia -t auto main.jl
 
 
