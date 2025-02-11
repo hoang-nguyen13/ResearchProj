50bcfc2c34feaf4cfcd4481d0e3446df1d206dbc
Hoang Nguyen <97536835+hoang-nguyen13@users.noreply.github.com>
2025-02-11 16:35:43 +0100

    Update slurm_script_original.sh

--- TWA/Rydberg_facilitation/slurm_script_original.sh
+++ TWA/Rydberg_facilitation/slurm_script_original.sh
@@ -7,30 +7,21 @@
 #SBATCH -e err/%x_%A_%a.err
 #SBATCH -o out/%x_%A_%a.out
 #SBATCH --mem 180G
-#SBATCH --array=0-19  # 5 omega chunks × 4 trajectory chunks = 20 jobs
+#SBATCH --array=0-1  # 2 chunks for 2 nodes
+#SBATCH --nodes=1    # Request 1 node per array task
 
-# Define chunks
+# Define omega chunks (split into 2)
 OMEGA_RANGES=(
-    "0 5" "5 10" "10 15" "15 20" "20 25"
+    "0 12.5"    # First half for node 1
+    "12.5 25"   # Second half for node 2
 )
 
-TRAJ_RANGES=(
-    "0 125" "125 250" "250 375" "375 500"
-)
 
-# Calculate omega and trajectory indices
-OMEGA_IDX=$((SLURM_ARRAY_TASK_ID / 4))
-TRAJ_IDX=$((SLURM_ARRAY_TASK_ID % 4))
+# Get omega range
+OMEGA_START=$(echo ${OMEGA_RANGES[$SLURM_ARRAY_TASK_ID]} | cut -d' ' -f1)
+OMEGA_END=$(echo ${OMEGA_RANGES[$SLURM_ARRAY_TASK_ID]} | cut -d' ' -f2)
 
-# Get ranges
-OMEGA_START=$(echo ${OMEGA_RANGES[$OMEGA_IDX]} | cut -d' ' -f1)
-OMEGA_END=$(echo ${OMEGA_RANGES[$OMEGA_IDX]} | cut -d' ' -f2)
-TRAJ_START=$(echo ${TRAJ_RANGES[$TRAJ_IDX]} | cut -d' ' -f1)
-TRAJ_END=$(echo ${TRAJ_RANGES[$TRAJ_IDX]} | cut -d' ' -f2)
-
-# Run Julia with both ranges
+# Run Julia with omega range only
 ~/julia-1.11.2/bin/julia -t auto main.jl \
     --omega-start $OMEGA_START \
-    --omega-end $OMEGA_END \
-    --traj-start $TRAJ_START \
-    --traj-end $TRAJ_END
+    --omega-end $OMEGA_END
