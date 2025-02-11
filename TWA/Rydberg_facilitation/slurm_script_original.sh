e92eb71f012b13b010fd63ebf7822594ef766d14
Hoang Nguyen <97536835+hoang-nguyen13@users.noreply.github.com>
2025-02-11 14:10:56 +0100

    Update slurm_script_original.sh

--- TWA/Rydberg_facilitation/slurm_script_original.sh
+++ TWA/Rydberg_facilitation/slurm_script_original.sh
@@ -4,10 +4,23 @@
 #SBATCH -c 36
 #SBATCH -t 50:00:00
 #SBATCH -p physik-fleischhauer
-#SBATCH -e err/%x_%A.err
-#SBATCH -o out/%x_%A.out
+#SBATCH -e err/%x_%A_%a.err
+#SBATCH -o out/%x_%A_%a.out
 #SBATCH --mem 180G
- 
-~/julia-1.11.2/bin/julia -t auto main.jl
+#SBATCH --array=0-4
 
+# Define Omega ranges for 5 chunks (adjust based on your needs)
+OMEGA_RANGES=(
+    "0 5"
+    "5 10"
+    "10 15"
+    "15 20"
+    "20 25"
+)
 
+# Get the range for this array task
+OMEGA_START=$(echo ${OMEGA_RANGES[$SLURM_ARRAY_TASK_ID]} | cut -d' ' -f1)
+OMEGA_END=$(echo ${OMEGA_RANGES[$SLURM_ARRAY_TASK_ID]} | cut -d' ' -f2)
+
+# Run Julia with the specific Omega range
+~/julia-1.11.2/bin/julia -t auto main.jl --omega-start $OMEGA_START --omega-end $OMEGA_END
