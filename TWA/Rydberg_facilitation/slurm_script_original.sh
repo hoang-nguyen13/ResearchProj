dbf4540c8879a5b96e6bff84d912a1d5281343fe
Hoang Nguyen <97536835+hoang-nguyen13@users.noreply.github.com>
2025-02-12 11:12:32 +0100

    Update slurm_script_original.sh

--- TWA/Rydberg_facilitation/slurm_script_original.sh
+++ TWA/Rydberg_facilitation/slurm_script_original.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 
 #SBATCH -J Rydberg
-#SBATCH -c 36
+#SBATCH -cpus-per-task=1
 #SBATCH -t 50:00:00
 #SBATCH -p physik-fleischhauer
 #SBATCH -e err/%x_%A_%a.err
 #SBATCH -o out/%x_%A_%a.out
-#SBATCH --mem 180G
-#SBATCH --array=0-50  # 51 values from 0 to 25 in steps of 0.5
+#SBATCH --mem-per-cpu 8G
+#SBATCH --array=0-25  
 
-# Single omega values array
-OMEGA_VALUES=(
-    0 0.5 1 1.5 2 2.5 3 3.5 4 4.5 5 
-    5.5 6 6.5 7 7.5 8 8.5 9 9.5 10 
-    10.5 11 11.5 12 12.5 13 13.5 14 14.5 
-    15 15.5 16 16.5 17 17.5 18 18.5 19 19.5 
-    20 20.5 21 21.5 22 22.5 23 23.5 24 24.5 25
-)
+id = ${SLURM_ARRAY_TASK_ID}
 
-# Get single omega value
-OMEGA=${OMEGA_VALUES[$SLURM_ARRAY_TASK_ID]}
-
-# Run Julia with single omega value
-~/julia-1.11.2/bin/julia -t auto main.jl --omega-start $OMEGA
+# Run Julia with omega range
+~/julia-1.11.2/bin/julia main.jl ${id}
